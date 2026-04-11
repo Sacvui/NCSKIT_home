e42f449a577158a2e6587456c7f867433dbff5e6
Phuc Hai Le <phuchai.le@gmail.com>
2026-04-11 08:44:11 +0700

    fix: refactor supabase client instantiation to resolve AbortError on scales data fetch

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,19 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const globalSupabase = global as unknown as {
-  supabaseClient: ReturnType<typeof createBrowserClient> | undefined
-}
-
 export const getSupabase = () => {
-  if (globalSupabase.supabaseClient) {
-    return globalSupabase.supabaseClient
-  }
-
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-  globalSupabase.supabaseClient = createBrowserClient(supabaseUrl, supabaseAnonKey)
-  return globalSupabase.supabaseClient
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
 
 export const createClient = getSupabase;
