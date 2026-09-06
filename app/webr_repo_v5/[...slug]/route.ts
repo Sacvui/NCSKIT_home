bbc83151bd0ce318d598432297a24e91ed306bb7
Hai <foreverlove3004@gmail.com>
2026-09-06 18:42:15 +0700

    clean up old buggy webr route

--- app/webr_repo_v5/[...slug]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    return new NextResponse("Not Found", { 
-        status: 404, 
-        headers: { 'Content-Type': 'text/plain' } 
-    });
-}
